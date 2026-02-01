fe1f7e2105e3984c3ab27d6495511d1faa5816e8
Abu Nusaybah <abunusaybah1>
2026-02-01 16:07:22 +0100

    COnfigured the storage settings that control the local storage

---
+++ lib/storage.ts
@@ -0,0 +1,26 @@
+import { DEFAULT_DATA } from "./defaults";
+import { PortfolioData } from "./types";
+
+const KEY = "portfolio_builder_data"
+
+export function saveData(data:PortfolioData) {
+    if (typeof window === undefined) return;
+    localStorage.setItem(KEY, JSON.stringify(data))
+}
+
+export function loadData(): PortfolioData{
+    if (typeof window === undefined) return DEFAULT_DATA;
+    const rawData = localStorage.getItem(KEY);
+    if (!rawData) return DEFAULT_DATA
+    
+    try {
+        return JSON.parse(rawData) as PortfolioData
+    } catch {
+        return DEFAULT_DATA
+    }
+}
+
+export function resetData() {
+    if (typeof window === undefined) return;
+    localStorage.removeItem(KEY)
+}
